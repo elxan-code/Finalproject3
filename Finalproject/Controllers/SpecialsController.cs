fba09034207aeb7673227f83088ec758459cd927
tu201806145 <tu201806145@213-13>
2022-03-09 18:40:27 +0400

    Add project files.

---
+++ Finalproject/Controllers/SpecialsController.cs
@@ -0,0 +1,28 @@
+﻿using Finalproject.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Finalproject.Controllers
+{
+    public class SpecialsController : Controller
+    {
+
+        private readonly AppDbContext _context;
+        public SpecialsController(AppDbContext context)
+        {
+            _context = context;
+
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Specials.ToList());
+        }
+        public IActionResult SpecialDeatails()
+        {
+            return View();
+        }
+    }
+}
---
+++ Finalproject/Migrations/20220309115505_init1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Finalproject.Migrations
+{
+    public partial class init1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DiscountDate",
+                table: "Specials");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Price",
+                table: "Specials",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Discount",
+                table: "Specials",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discountfaiz",
+                table: "Specials",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discountfaiz",
+                table: "Specials");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Price",
+                table: "Specials",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Discount",
+                table: "Specials",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DiscountDate",
+                table: "Specials",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
